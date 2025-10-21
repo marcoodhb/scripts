ef79a04affb0ef186419a8b0edaa78c77cd2b995
marcoodhb <chahuaram13@gmail.com>
2025-10-21 05:39:14 -0500

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -8,7 +8,6 @@ tar -xzf "$TOOLCHAIN_ARCHIVE_PATH" -C "$TOOLCHAIN_EXTRACT_DIR"
 
 echo "...Actualizando submódulos del kernel..."
 cd kernel/xiaomi/earth
-git remote prune origin
 git submodule sync --recursive
 git submodule update --init --recursive
 cd ../../..
